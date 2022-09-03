b5dd12cdd254ba3f2311a60b4066f9f4d12bb857
Marcel Kuhla <marcelkuhla@outlook.com>
2022-09-03 17:38:25 +0200

    5th Update

--- cars.php
+++ cars.php
@@ -3,6 +3,8 @@
 // Initialize the language-module
 $_language->read_module('cars');
 
+$filepath = "./uploads/cars/";
+
 if(isset($_GET['carID'])) $userID = $_GET['carID'];
 else $carID = '';
 
@@ -20,22 +22,45 @@ if($action == 'new'){
 }elseif($action == 'save'){
     if($_POST['last'] == 'new'){
         $name = $_POST['name'];
-        $manufacturer = $_POST['manufacturer'];
-        
+        $handle = $_POST['handle'];
         $year = $_POST['year'];
+        $manufacturer = $_POST['manufacturer'];
         $value = $_POST['value'];
 
-        $picture = $_POST['picture'];
-        $handle = $_POST['handle'];
+        $dlc = $_POST['dlc'];
+        $gconly = $_POST['gconly'];
+        $lottery = $_POST['lottery'];
+
+        $picture = $_FILES['picture'];
+        $file_ext=strtolower(mb_substr($picture['name'], strrpos($picture['name'], ".")));
+        if($file_ext==".png" OR $file_ext==".jpg") {
+            move_uploaded_file($picture['tmp_name'], $filepath.$picture['name']);
+            @chmod($filepath.$picture['name'], 0755);
+            $file = $handle.$file_ext;
+            rename($filepath.$picture['name'], $filepath.$file);
+            $picture = $file;
+
+        }
+
+        $copyright = $_POST['copyright'];
 
         $newCar = [
-            "manufacturer" => "$manufacturer",
-            "manufacturerID" => "$manufacturerID",
             "name" => "$name",
-            "value" => "$value",
-            "year" => "$year",
-            "picture" => "$picture",
             "handle" => "$handle",
+            "attributes" => [
+                "year" => "$year",
+                "manufacturer" => "$manufacturer",
+                "value" => "$value",
+            ],
+            "extras" => [
+                "dlc" => "$dlc",
+                "gconly" => "$gconly",
+                "lottery" => "$lottery"
+            ],
+            "graphics" => [
+                "picture" => "$picture",
+                "copyright" => "$copyright",
+            ],
         ];
         $newCar = $carsStore->insert($newCar);
 
@@ -58,6 +83,8 @@ if($action == 'new'){
             foreach($cars as $car){
 
                 $name = $car['name'];
+                $handle = $car['handle'];
+                $manufacturer = $car['attributes']['manufacturer'];
                 $id = $car['_id'];
 
                 $actions = '<a class="button small secondary" href="admin.php?site=cars&action=edit&carID='.$id.'">'.$_language->module['edit'].'</a>';
